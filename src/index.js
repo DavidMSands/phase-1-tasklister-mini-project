45a446ee13879f9c9453431b00937540a494a1b3
David <Davidmaxsands@gmail.com>
2022-01-17 18:01:11 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector("form")
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      buildToDo(e.target.new_task_description.value)
+      form.reset()
+    })
 });
+
+const buildToDo = (todo) => {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', (e) => {
+    e.target.parentNode.remove()
+  })
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  document.getElementById("list").appendChild(li)
+}
+
+
