b9c4beb4e58f14454f4b2a114f6f28e0bc737850
David <Davidmaxsands@gmail.com>
2022-01-18 11:58:24 -0700

    added delete all button and checkbox

--- src/index.js
+++ src/index.js
@@ -10,13 +10,32 @@ document.addEventListener("DOMContentLoaded", () => {
 const buildToDo = (todo) => {
   let li = document.createElement('li')
   let btn = document.createElement('button')
+  let check = document.createElement('input')
+  let label = document.createElement('label')
+  check.id = "check";
+  check.type = "checkbox"
+  label.setAttribute("for", "check")
   btn.addEventListener('click', (e) => {
     e.target.parentNode.remove()
   })
   btn.textContent = 'x'
-  li.textContent = `${todo} `
+  label.textContent = `${todo} `
+  li.appendChild(check)
+  li.appendChild(label)
   li.appendChild(btn)
-  document.getElementById("list").appendChild(li)
+  document.getElementById("tasks").appendChild(li)
 }
+
+let tasks = document.getElementById("tasks")
+let deleteBtn = document.getElementById("deleteAll")
+
+deleteBtn.addEventListener('click', () => {
+   while (tasks.firstChild) {
+     tasks.removeChild(tasks.firstChild)
+   }
+})
+
+
+
 
 
